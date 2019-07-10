273a444cfefc721fc64a92d8f242b9da5482ff17
Rad Azzouz <rad@pspdfkit.com>
2019-07-10 13:43:59 -0400

    Run prettier on PSPDFKit.ios.js

--- www/PSPDFKit.ios.js
+++ www/PSPDFKit.ios.js
@@ -10,209 +10,222 @@
 //  This notice may not be removed from this file.
 //
 
-var PSPDFKitPlugin = new function() {
-    
-    // Utilities
-    
-    var self = this;
-    function addMethods(methods) {
-        for (var name in methods) {
-            (function() {
-                var methodName = name;
-                var methodArgs = methods[methodName];
-                self[methodName] = function() {
-                    var callback = null;
-                    var argArray = [];
-                    for (var i = 0; i < arguments.length; i++) {
-                        argArray.push(arguments[i]);
-                        if (methodArgs[i] == 'callback') {
-                            if (typeof (arguments[i]) == "function") {
-                                callback = arguments[i];
-                            }
-                        }
-                    }
-                    cordova.exec(function (result) {
-                        if (callback) callback(result, null);
-                    }, function (error) {
-                        console.log(error);
-                        if (callback) callback(null, error);
-                    }, 'PSPDFKitPlugin', methodName, argArray);
-                }
-            })();
-        }
-    }
-    
-    // Events
-    
-    var listeners = {};
-    
-    this.dispatchEvent = function(event) {
-        var result = undefined;
-        var functions = listeners[event.type];
-        if (functions) {
-            for (var i = 0; i < functions.length; i++) {
-                result = functions[i](event);
-                if (typeof result != 'undefined') {
-                    if (!result) return result;
-                }
+var PSPDFKitPlugin = new (function() {
+  // Utilities
+
+  var self = this;
+  function addMethods(methods) {
+    for (var name in methods) {
+      (function() {
+        var methodName = name;
+        var methodArgs = methods[methodName];
+        self[methodName] = function() {
+          var callback = null;
+          var argArray = [];
+          for (var i = 0; i < arguments.length; i++) {
+            argArray.push(arguments[i]);
+            if (methodArgs[i] == "callback") {
+              if (typeof arguments[i] == "function") {
+                callback = arguments[i];
+              }
             }
+          }
+          cordova.exec(
+            function(result) {
+              if (callback) callback(result, null);
+            },
+            function(error) {
+              console.log(error);
+              if (callback) callback(null, error);
+            },
+            "PSPDFKitPlugin",
+            methodName,
+            argArray
+          );
+        };
+      })();
+    }
+  }
+
+  // Events
+
+  var listeners = {};
+
+  this.dispatchEvent = function(event) {
+    var result = undefined;
+    var functions = listeners[event.type];
+    if (functions) {
+      for (var i = 0; i < functions.length; i++) {
+        result = functions[i](event);
+        if (typeof result != "undefined") {
+          if (!result) return result;
         }
-        return result;
+      }
     }
+    return result;
+  };
 
-    this.addEventListener = function(type, listener) {
-        var existing = listeners[type];
-        if (!existing) {
-            existing = [];
-            listeners[type] = existing;
-        }
-        existing.push(listener);
+  this.addEventListener = function(type, listener) {
+    var existing = listeners[type];
+    if (!existing) {
+      existing = [];
+      listeners[type] = existing;
     }
+    existing.push(listener);
+  };
 
-    this.addEventListeners = function(listeners) {
-        for (type in listeners) {
-            this.addEventListener(type, listeners[type]);
-        }
+  this.addEventListeners = function(listeners) {
+    for (type in listeners) {
+      this.addEventListener(type, listeners[type]);
     }
+  };
 
-    this.removeEventListener = function(type, listener)
-    {
-        var existing = listeners[type];
-        if (existing) {
-            var index;
-            while ((index = existing.indexOf(listener)) != -1) {
-                existing.splice(index,1);
-            }
-        }
+  this.removeEventListener = function(type, listener) {
+    var existing = listeners[type];
+    if (existing) {
+      var index;
+      while ((index = existing.indexOf(listener)) != -1) {
+        existing.splice(index, 1);
+      }
     }
+  };
 
-    // License key
-    
-    addMethods({
-        setLicenseKey: ['key'],
-    });
+  // License key
 
-    // PDF Generation method
-    
-    addMethods({
-        convertPDFFromHTMLString: ['html', 'fileName', 'options', 'callback'],
-    });
-    
-    // Document methods
-    
-    addMethods({
-        present: ['path', 'callback', 'options'],
-        presentWithXFDF: ['path', 'xfdfPath', 'callback', 'options'],
-        dismiss: ['callback'],
-        reload: [],
-        search: ['query', 'animated', 'headless'],
-        saveAnnotations: ['callback'],
-        getHasDirtyAnnotations: ['callback'],
-    });
-    
-    // Configuration
-    
-    addMethods({
-        setOptions: ['options', 'animated'],
-        getOptions: ['names', 'callback'],
-        setOption: ['name', 'value', 'animated'],
-        getOption: ['name', 'callback'],
-    });
-    
-    // Page scrolling
-    
-    addMethods({
-        setPage: ['page', 'animated'],
-        getPage: ['callback'],
-        getScreenPage: ['callback'],
-        getPageCount: ['callback'],
-        scrollToNextPage: ['animated'],
-        scrollToPreviousPage: ['animated'],
-    });
+  addMethods({
+    setLicenseKey: ["key"]
+  });
 
-    // Appearance
-    
-    addMethods({
-        setAppearanceMode: ['appearanceMode'],
-    });
+  // PDF Generation method
 
-    // Cache
+  addMethods({
+    convertPDFFromHTMLString: ["html", "fileName", "options", "callback"]
+  });
 
-    addMethods({
-        clearCache: [],
-        removeCacheForPresentedDocument: [],
-    });
+  // Document methods
 
-    // Toolbar
-    
-    var leftBarButtonItems = ['close'];
-    var rightBarButtonItems = ['search', 'outline', 'thumbnails'];
-    
-    this.dispatchLeftBarButtonAction = function(index)
-    {
-        leftBarButtonItems[index].action();
-    }
+  addMethods({
+    present: ["path", "callback", "options"],
+    presentWithXFDF: ["path", "xfdfPath", "callback", "options"],
+    dismiss: ["callback"],
+    reload: [],
+    search: ["query", "animated", "headless"],
+    saveAnnotations: ["callback"],
+    getHasDirtyAnnotations: ["callback"]
+  });
 
-    this.dispatchRightBarButtonAction = function(index)
-    {
-        rightBarButtonItems[index].action();
-    }
+  // Configuration
 
-    this.setLeftBarButtonItems = function(items)
-    {
-        leftBarButtonItems = items;
-        cordova.exec(function (result) { }, function (error) { },
-                     'PSPDFKitPlugin', 'setLeftBarButtonItems', [items]);
-    }
+  addMethods({
+    setOptions: ["options", "animated"],
+    getOptions: ["names", "callback"],
+    setOption: ["name", "value", "animated"],
+    getOption: ["name", "callback"]
+  });
 
-    this.setRightBarButtonItems = function(items)
-    {
-        rightBarButtonItems = items;
-        cordova.exec(function (result) { }, function (error) { },
-                     'PSPDFKitPlugin', 'setRightBarButtonItems', [items]);
-    }
+  // Page scrolling
 
-    this.getLeftBarButtonItems = function(callback)
-    {
-        callback(leftBarButtonItems);
-    }
-    
-    this.getRightBarButtonItems = function(callback)
-    {
-        callback(rightBarButtonItems);
-    }
+  addMethods({
+    setPage: ["page", "animated"],
+    getPage: ["callback"],
+    getScreenPage: ["callback"],
+    getPageCount: ["callback"],
+    scrollToNextPage: ["animated"],
+    scrollToPreviousPage: ["animated"]
+  });
 
-    // Annotation toolbar
-    addMethods({
-        hideAnnotationToolbar: [],
-        showAnnotationToolbar: [],
-        toggleAnnotationToolbar: [],
-    });
-    
-    // Instant JSON
-    addMethods({
-        applyInstantJSON: ['jsonValue', 'callback'],
-        addAnnotation: ['jsonAnnotation', 'callback'],
-        removeAnnotation: ['jsonAnnotation', 'callback'],
-        getAnnotations: ['pageIndex', 'type', 'callback'],
-        getAllUnsavedAnnotations: ['callback']
-    });
-    
-    // Forms
-    addMethods({
-        setFormFieldValue: ['value', 'fullyQualifiedName', 'callback'],
-        getFormFieldValue: ['fullyQualifiedName', 'callback'],
-    });
-    
-    // XFDF
-    addMethods({
-        importXFDF: ['xfdfPath', 'callback'],
-        exportXFDF: ['xfdfPath', 'callback'],
-    });
-	
-    // Document Processing
-    addMethods({
-        processAnnotations: ['annotationChange', 'processedDocumentPath', 'callback', 'annotationType'],
-    });
-};
+  // Appearance
+
+  addMethods({
+    setAppearanceMode: ["appearanceMode"]
+  });
+
+  // Cache
+
+  addMethods({
+    clearCache: [],
+    removeCacheForPresentedDocument: []
+  });
+
+  // Toolbar
+
+  var leftBarButtonItems = ["close"];
+  var rightBarButtonItems = ["search", "outline", "thumbnails"];
+
+  this.dispatchLeftBarButtonAction = function(index) {
+    leftBarButtonItems[index].action();
+  };
+
+  this.dispatchRightBarButtonAction = function(index) {
+    rightBarButtonItems[index].action();
+  };
+
+  this.setLeftBarButtonItems = function(items) {
+    leftBarButtonItems = items;
+    cordova.exec(
+      function(result) {},
+      function(error) {},
+      "PSPDFKitPlugin",
+      "setLeftBarButtonItems",
+      [items]
+    );
+  };
+
+  this.setRightBarButtonItems = function(items) {
+    rightBarButtonItems = items;
+    cordova.exec(
+      function(result) {},
+      function(error) {},
+      "PSPDFKitPlugin",
+      "setRightBarButtonItems",
+      [items]
+    );
+  };
+
+  this.getLeftBarButtonItems = function(callback) {
+    callback(leftBarButtonItems);
+  };
+
+  this.getRightBarButtonItems = function(callback) {
+    callback(rightBarButtonItems);
+  };
+
+  // Annotation toolbar
+  addMethods({
+    hideAnnotationToolbar: [],
+    showAnnotationToolbar: [],
+    toggleAnnotationToolbar: []
+  });
+
+  // Instant JSON
+  addMethods({
+    applyInstantJSON: ["jsonValue", "callback"],
+    addAnnotation: ["jsonAnnotation", "callback"],
+    removeAnnotation: ["jsonAnnotation", "callback"],
+    getAnnotations: ["pageIndex", "type", "callback"],
+    getAllUnsavedAnnotations: ["callback"]
+  });
+
+  // Forms
+  addMethods({
+    setFormFieldValue: ["value", "fullyQualifiedName", "callback"],
+    getFormFieldValue: ["fullyQualifiedName", "callback"]
+  });
+
+  // XFDF
+  addMethods({
+    importXFDF: ["xfdfPath", "callback"],
+    exportXFDF: ["xfdfPath", "callback"]
+  });
+
+  // Document Processing
+  addMethods({
+    processAnnotations: [
+      "annotationChange",
+      "processedDocumentPath",
+      "callback",
+      "annotationType"
+    ]
+  });
+})();
 module.exports = PSPDFKitPlugin;
