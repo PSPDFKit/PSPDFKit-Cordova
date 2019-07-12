b19e8d488a45bbc30a6af1186f8cf0d771f1524e
anastasia-zaitsewa <anastasia.zaitsewa@gmail.com>
2019-07-12 20:58:35 +0200

    Merging gitignore and copying android source

---
+++ src/android/java/com/pspdfkit/cordova/CordovaPdfActivity.java
@@ -0,0 +1,148 @@
+package com.pspdfkit.cordova;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.pspdfkit.cordova.event.EventDispatcher;
+import com.pspdfkit.document.PdfDocument;
+import com.pspdfkit.listeners.DocumentListener;
+import com.pspdfkit.listeners.SimpleDocumentListener;
+import com.pspdfkit.ui.PdfActivity;
+import com.pspdfkit.ui.PdfFragment;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
+import static com.pspdfkit.cordova.Utilities.checkArgumentNotNull;
+
+public class CordovaPdfActivity extends PdfActivity {
+
+  /**
+   * For communication with the JavaScript context, we keep a static reference to the current
+   * activity.
+   */
+  private static CordovaPdfActivity currentActivity;
+  private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+  @NonNull
+  private final DocumentListener listener =
+      new SimpleDocumentListener() {
+        @Override
+        public void onDocumentSaved(@NonNull PdfDocument document) {
+          EventDispatcher.getInstance().sendEvent("onDocumentSaved", new JSONObject());
+          Log.d("WTF", "onDocumentSaved");
+        }
+
+        @Override
+        public void onDocumentSaveFailed(
+            @NonNull PdfDocument document, @NonNull Throwable exception) {
+          Log.d("WTF", "onDocumentSaveFailed");
+          try {
+            final JSONObject data = new JSONObject();
+            data.put("message", exception.getMessage());
+            EventDispatcher.getInstance().sendEvent("onDocumentSaveFailed", data);
+          } catch (JSONException e) {
+            e.printStackTrace();
+          }
+        }
+
+        @Override
+        public void onDocumentLoaded(@NonNull PdfDocument document) {
+          Log.d("WWTF", "onDocumentLoaded");
+        }
+      };
+
+  public static CordovaPdfActivity getCurrentActivity() {
+    return currentActivity;
+  }
+
+  private void bindActivity(@NonNull final CordovaPdfActivity activity) {
+    checkArgumentNotNull(activity, "activity");
+    if (currentActivity != null) {
+      throw new IllegalStateException(
+          "EventDispatcher only supports a single CordovaPdfActivity at a time.");
+    }
+    currentActivity = activity;
+    final PdfFragment pdfFragment = currentActivity.getPdfFragment();
+    if (pdfFragment == null) {
+      throw new IllegalStateException(
+          "EventDispatcher only supports binding to activities that have a fragment instance.");
+    }
+
+    Log.d("WTF", "listener during create = " + listener);
+
+    pdfFragment.addDocumentListener(listener);
+  }
+
+  private void releaseActivity() {
+    PdfFragment fragment = currentActivity.getPdfFragment();
+    if (fragment != null) {
+      fragment.removeDocumentListener(listener);
+    }
+    currentActivity = null;
+  }
+
+  /**
+   * Adds given {@link Disposable} to disposable collection which will be automatically disposed as
+   * a part of onDestroy during activity finishing process.
+   *
+   * @param disposable to add to the disposable collection.
+   */
+  public void addSubscription(Disposable disposable) {
+    compositeDisposable.add(disposable);
+  }
+
+  private void disposeSubscriptions() {
+    compositeDisposable.dispose();
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    bindActivity(this);
+  }
+
+  @Override
+  protected void onDestroy() {
+    releaseActivity();
+
+    if (isFinishing()) {
+      disposeSubscriptions();
+    }
+
+    super.onDestroy();
+  }
+
+  @Override
+  public void finish() {
+    super.finish();
+    // Notify JavaScript listeners that the activity was dismissed.
+    EventDispatcher.getInstance().notifyActivityDismissed();
+  }
+
+  /**
+   * Dismisses this PDF activity.
+   */
+  public void dismiss() {
+    finish();
+  }
+
+  public boolean saveDocument() throws IOException {
+    final PdfDocument document = getDocument();
+    if (document != null) {
+      boolean modified = document.saveIfModified();
+      Log.d("WTF", "wasModified  + " + modified);
+      Log.d("WTF", "listener during save = " + listener);
+      return modified;
+    } else {
+      return false;
+    }
+  }
+}
